942c5362abd3785bae7cf5789e596540e8beb25f
Bobi <p.bobvos@gmail.com>
2016-01-14 22:00:11 +0100

    #1 Create welcome page
    #3 Create application base (MEAN)

---
+++ index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function (req, res) {
+	var _url;
+	req.method = req.method.toUpperCase();
+	console.log(req.method + ' ' + req.url);
+	res.end('The current time is ' + Date.now())
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http: //127. 0. 0. 1: 1337/');
